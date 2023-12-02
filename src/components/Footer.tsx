a585b7976662b8649e6e33c277c67c0b7344f8da
Pyae Phyo Han <pyaephyohan2200@gmail.com>
2023-12-02 21:18:56 +0630

    added social media links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,20 +4,22 @@ const Footer = () => {
   return (
     <div className="md:flex items-center justify-between md:px-12 p-4 py-[2rem] bg-primary text-white">
       <div className="text-center">
-        <p className="text-xl font-bold">@Coffee Hub. All rights reserved.</p>
+        <p className="text-lg md:text-xl font-bold">
+          @Coffee Hub. All rights reserved.
+        </p>
       </div>
       <div className="flex space-x-6 mt-[1.8rem] md:mt-0 justify-center">
-        <a href="/">
-          <FaFacebook className="text-4xl text-[#0866FF]" />
+        <a href="https://www.facebook.com/profile.php?id=100048688099795">
+          <FaFacebook className="text-4xl" />
         </a>
-        <a href="/">
-          <FaInstagram className="text-4xl text-[#833AB4]" />
+        <a href="https://www.instagram.com/pyaephyohan0807">
+          <FaInstagram className="text-4xl" />
         </a>
-        <a href="/">
+        <a href="https://github.com/pyaephyohann">
           <FaGithub className="text-4xl" />
         </a>
-        <a href="/">
-          <FaLinkedin className="text-4xl text-[#0A66C2]" />
+        <a href="https://www.linkedin.com/in/pyae-phyo-han-65aab2264/">
+          <FaLinkedin className="text-4xl" />
         </a>
       </div>
     </div>
